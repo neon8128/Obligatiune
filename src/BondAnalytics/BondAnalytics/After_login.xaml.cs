4211917fbccfd5c62db7566b5d170b4723bc84c4
neon8128 <suciudavid6@gmail.com>
2020-07-27 18:10:57 +0300

    mesaj

---
+++ src/BondAnalytics/BondAnalytics/After_login.xaml.cs
@@ -0,0 +1,190 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BondAnalytics
+{
+    /// <summary>
+    /// Interaction logic for After_login.xaml
+    /// </summary>
+    public partial class After_login : Window
+    {
+        int attempts = 0;
+        private string user;
+        private string acquired_pass;
+
+
+
+
+
+        public After_login(string user, string acquired_pass)
+        {
+            InitializeComponent();
+            this.user = user;
+            this.acquired_pass = acquired_pass;
+
+        }
+
+
+
+        public bool Verify(string user, string pass)
+        {
+            string test_pass = null;
+            string connection = "server=localhost;port=3306;uid=" + user + ";pwd=" + acquired_pass + ";database=bond;charset=utf8;SslMode=none";
+
+            using (MySqlConnection conn = new MySqlConnection(connection))
+            {
+                conn.Open();
+
+                using (var cmd = new MySqlCommand("SELECT password FROM user where username=@user", conn))
+                {
+                    cmd.Parameters.AddWithValue("@user", user);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            test_pass = reader.GetString(0);
+                        }
+                    }
+                    conn.Close();
+                    if (test_pass == pass)
+                        return true;
+                }
+                return false;
+            }
+        }
+        public bool Verify_with_credentials(string user)
+        {
+
+            string test_pass = null;
+            string connection = "server=localhost;port=3306;uid=" + user + ";pwd=" + acquired_pass + ";database=bond;charset=utf8;SslMode=none";
+
+            using (MySqlConnection conn = new MySqlConnection(connection))
+            {
+                conn.Open();
+
+                using (var cmd = new MySqlCommand("SELECT password FROM user where username=@user", conn))
+                {
+                    cmd.Parameters.AddWithValue("@user", user);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            test_pass = reader.GetString(0);
+                        }
+                    }
+                    conn.Close();
+                    if (test_pass == acquired_pass)
+                        return true;
+                }
+                return false;
+            }
+        }
+
+        private void c1_Checked(object sender, RoutedEventArgs e)
+        {
+            // if (c1.IsChecked ?? false)
+            // {
+            //     MessageBox.Show(password.Password);
+            // }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (Verify(user, password.Password))
+            {
+                MessageBox.Show("You have successfully logged in as " + user);
+                Insert_audit();
+                this.Close();
+                Real_main_window r1 = new Real_main_window(user, acquired_pass);
+                r1.Show();
+
+            }
+
+            else
+            {
+                attempts++;
+                MessageBox.Show("Try again!" + "\n" + "You have tried " + attempts + " out of 3");
+
+
+            }
+
+            if (attempts == 3)
+            {
+                MessageBox.Show("Bye");
+                Application.Current.Shutdown();
+            }
+        }
+
+        public void Insert_audit()
+        {
+            string ip = GetLocalIPAddress();
+            string pc = System.Environment.MachineName;
+            DateTime time = DateTime.Now;
+            string format = "d M yyyy HH:mm:s ";
+
+
+            string connection = "server=localhost;port=3306;uid=" + user + ";pwd=" + acquired_pass + ";database=bond;charset=utf8;SslMode=none";
+
+            using (MySqlConnection conn = new MySqlConnection(connection))
+            {
+                conn.Open();
+
+                using (var cmd = new MySqlCommand("INSERT INTO `audit`(`username`, `TS`, `details`, `machine_name`, `ip`) VALUES (@username,@TS,@details,@machine,@ip)", conn))
+                {
+                    cmd.Parameters.AddWithValue("@username", user.ToString());
+                    cmd.Parameters.AddWithValue("@TS", time.ToString(format));
+                    cmd.Parameters.AddWithValue("@details", "user logged in");
+                    cmd.Parameters.AddWithValue("@machine", pc.ToString());
+                    cmd.Parameters.AddWithValue("@ip", ip.ToString());
+                    cmd.ExecuteNonQuery();
+
+                    conn.Close();
+
+                }
+
+            }
+
+
+
+
+
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+    }
+}
---
+++ src/BondAnalytics/BondAnalytics/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using CredentialManagement;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BondAnalytics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int i = 0;
+        bool ok = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool Connect(string a, string b)
+        {
+            string connection = "server=localhost;port=3306;uid=" + a + ";pwd=" + b + ";database=bond;charset=utf8;SslMode=none";
+
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(connection))
+                {
+                    con.Open();
+
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+
+                return false;
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string acquired_pass = GetCredential("Bond_calculator", user.Text);
+
+            //  Debug.Assert(GetCredential("Bond_calculator") == null);
+
+            if (acquired_pass != null)
+            {
+                MessageBox.Show("Your password was saved in WCM, let me grab it");
+                MessageBox.Show("HI " + user.Text + "!" + Environment.NewLine + "Now introduce your password for the app");
+                this.Hide();
+                After_login after_Login = new After_login(user.Text, acquired_pass);
+                after_Login.Show();
+                ok = true;
+
+            }
+            if (Connect(user.Text, pass.Password))
+            {
+                MessageBox.Show("HI " + user.Text + "!" + Environment.NewLine + "Now introduce your password for the app");
+                this.Hide();
+                After_login after_Login = new After_login(user.Text, pass.Password.ToString());
+                after_Login.Show();
+                SetCredentials("Bond_calculator", user.Text, pass.Password, PersistanceType.LocalComputer);
+                ok = true;
+
+            }
+            else
+            {
+
+                if (ok == false)
+                {
+                    i++;
+                    MessageBox.Show("Try again!" + "\n" + "You have tried " + i + " out of 3");
+                    this.Close();
+                }
+
+
+
+            }
+
+            if (i >= 3)
+            {
+                MessageBox.Show("Bye");
+                Application.Current.Shutdown();
+            }
+
+
+        }
+
+        private void SetCredentials(string v, string text, string password, object localComputer)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void c1_Checked(object sender, RoutedEventArgs e)
+        {
+
+            // if (c1.IsChecked ?? false)
+            // {
+            //     MessageBox.Show(pass_text.Password);
+            // }
+        }
+        public static bool SetCredentials(string target, string username, string password, PersistanceType persistenceType)
+        {
+
+            return new Credential
+            {
+                Target = target,
+                Username = username,
+                Password = password,
+                PersistanceType = persistenceType
+            }.Save();
+        }
+        public static string GetCredential(string target, string user)
+        {
+            var cm = new Credential
+            {
+                Target = target,
+                Username = user
+            };
+            if (!cm.Load())
+            {
+                return null;
+            }
+
+            if (user == cm.Username)
+                return cm.Password;
+            else return null;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
